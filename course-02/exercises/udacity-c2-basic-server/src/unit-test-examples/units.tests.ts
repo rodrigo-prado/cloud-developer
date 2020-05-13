8175cdad20111a6e9658e7ec27b4005c67033a44
Rodrigo-Alves <Rodrigo.Alves@ibm.com>
2020-05-13 17:54:38 -0300

    All changes for the class 5 of the Cloud Developer at Udacity

--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
@@ -1,4 +1,4 @@
-import { add, divide } from './units';
+import { add, divide, concat } from './units';
 
 import { expect } from 'chai';
 import 'mocha';
@@ -38,3 +38,19 @@ describe('divide', () => {
 // @TODO try creating a new describe block for the "concat" method
 // it should contain an it block for each it statement in the units.ts @TODO.
 // don't forget to import the method ;)
+
+describe('concat', () => {
+  it('should take two parameters', () => {
+    const result = concat('First', 'Second');
+    // expect(result).to.equal('FirstSecond');
+  });
+
+  it("should concat 'First' and 'Second'", () => {
+    const result = concat('First', 'Second');
+    expect(result).to.equal('FirstSecond');
+  });
+
+  it('should throw an error if either of the strings are empty', () => {
+    expect(()=>{ concat('', 'other') }).to.throw('One or both strings are empty')
+  });
+});
--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -9,22 +9,74 @@ const router: Router = Router();
 router.get('/', async (req: Request, res: Response) => {
     const items = await FeedItem.findAndCountAll({order: [['id', 'DESC']]});
     items.rows.map((item) => {
-            if(item.url) {
-                item.url = AWS.getGetSignedUrl(item.url);
-            }
+        if (item.url) {
+            item.url = AWS.getGetSignedUrl(item.url);
+        }
     });
     res.send(items);
 });
 
 //@TODO
 //Add an endpoint to GET a specific resource by Primary Key
+router.get('/:id',
+    async (req: Request, res: Response) => {
+        // destruct our path params
+        let { id } = req.params;
+
+        // check to make sure the id is set
+        if (!id) {
+            return res.status(400).send(`id is required`);
+        }
+
+        // try to found feed by id
+        const feed = await FeedItem.findByPk(id);
+
+        res.send(feed);
+    }
+);
 
 // update a specific resource
 router.patch('/:id', 
     requireAuth, 
     async (req: Request, res: Response) => {
+        // destruct our path params
+        let { id } = req.params;
+        const caption = req.body.caption;
+        const fileName = req.body.url;
+
+        // check to make sure the id is set
+        if (!id) {
+            return res.status(400).send(`id is required`);
+        }
+
+        // try to found feed by id
+        const feed = await FeedItem.findByPk(id);
+
+        // check Caption is valid
+        if (!caption) {
+            return res.status(400).send({ message: 'Caption is required or malformed' });
+        }
+
+        // check Filename is valid
+        if (!fileName) {
+            return res.status(400).send({ message: 'File url is required' });
+        }
+
+        feed.caption = caption;
+        feed.url = fileName;
+        // const item = await new FeedItem({
+        //     caption: caption,
+        //     url: fileName
+        // });
+
+        // const saved_item = await item.save();
+        const saved_item = await feed.save();
+
+        saved_item.url = AWS.getGetSignedUrl(saved_item.url);
+        res.status(200).send(saved_item);
+
         //@TODO try it yourself
-        res.send(500).send("not implemented")
+        // res.send(501).send("not implemented")
 });
 
 
